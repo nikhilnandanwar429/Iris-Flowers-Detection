fc7746cb8cac941175c4007d944d7b3a8c853272
nischay59 <narendrasaryam9459@gmail.com>
2025-04-18 18:22:13 +0530

    ml model update

--- server/iris.py
+++ server/iris.py
@@ -1,21 +1,20 @@
+# iris.py
 import pandas as pd
-import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 import pickle
 
 df = pd.read_csv('iris.data')
+X = df.iloc[:, 0:4].values
+y = df.iloc[:, 4].values
 
-X = np.array(df.iloc[:, 0:4])
-y = np.array(df.iloc[:, 4:])
-
-from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
-y = le.fit_transform(y.reshape(-1))
+y = le.fit_transform(y)
 
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = SVC(kernel='linear').fit(X_train, y_train)
 
-from sklearn.svm import SVC
-sv = SVC(kernel='linear').fit(X_train,y_train)
-
-
-pickle.dump(sv, open('iri.pkl', 'wb'))
+# Save with protocol=4 for compatibility
+with open('iri.pkl', 'wb') as f:
+    pickle.dump(model, f, protocol=4)
