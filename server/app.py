5e2cc235ccff6d89175a648c638342fcd4dbdb95
nischay59 <narendrasaryam9459@gmail.com>
2025-04-18 17:09:14 +0530

    reqi changes

--- server/app.py
+++ server/app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import numpy as np
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 model = pickle.load(open('iri.pkl', 'rb'))
 
@@ -22,4 +26,5 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(debug=True, port=port)
